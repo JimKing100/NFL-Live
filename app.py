dccf21696db19789f15b8061f2bb5ae0d8262f0b
Jim King <52686228+JimKing100@users.noreply.github.com>
2020-05-13 10:11:33 -0700

    Add the application and index files
    Add the python application and index files

---
+++ app.py
@@ -0,0 +1,7 @@
+import dash
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.config.suppress_callback_exceptions = True
+server = app.server
---
+++ index.py
@@ -0,0 +1,28 @@
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+
+from app import app, server
+from tabs import predict, explain, evaluate
+
+style = {'maxWidth': '960px', 'margin': 'auto'}
+
+app.layout = html.Div([
+    dcc.Markdown('NFL Fantasy Football Trade Analyzer'),
+    dcc.Tabs(id='tabs', value='tab-predict', children=[
+        dcc.Tab(label='Predict', value='tab-predict'),
+        dcc.Tab(label='Explain', value='tab-explain'),
+        dcc.Tab(label='Evaluate', value='tab-evaluate'),
+    ]),
+    html.Div(id='tabs-content'),
+], style=style)
+
+@app.callback(Output('tabs-content', 'children'),
+              [Input('tabs', 'value')])
+def render_content(tab):
+    if tab == 'tab-predict': return predict.layout
+    elif tab == 'tab-explain': return explain.layout
+    elif tab == 'tab-evaluate': return evaluate.layout
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
