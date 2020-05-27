54062e9105dbb7702019bfb6a64ae33cc7d7efba
Jim King <52686228+JimKing100@users.noreply.github.com>
2020-05-26 18:31:55 -0700

    Add Methodology and Evaluation Info

--- tabs/evaluate.py
+++ tabs/evaluate.py
@@ -1,9 +1,71 @@
 import dash_core_components as dcc
-
-layout = [dcc.Markdown("""
-### Final Results
-
-The prediction evaluation
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
 
 
-""")]
+# Convert the data to tidy format
+def convert_data(df, col_name, col_nn, col_arima, col_avg):
+    column_names = ['Week', 'Model', col_name]
+    graph_df = pd.DataFrame(columns=column_names)
+
+    for i in range(1, 18):
+        week = df['week'].loc[(df['week'] == i)].iloc[0]
+        model = 'NN'
+        result = df[col_nn].loc[(df['week'] == i)].iloc[0]
+        graph_df = graph_df.append({'Week': week,
+                                    'Model': model,
+                                    col_name: result
+                                    }, ignore_index=True)
+
+        model = 'ARIMA'
+        result = df[col_arima].loc[(df['week'] == i)].iloc[0]
+        graph_df = graph_df.append({'Week': week,
+                                    'Model': model,
+                                    col_name: result
+                                    }, ignore_index=True)
+
+        model = 'Average'
+        result = df[col_avg].loc[(df['week'] == i)].iloc[0]
+        graph_df = graph_df.append({'Week': week,
+                                    'Model': model,
+                                    col_name: result
+                                    }, ignore_index=True)
+
+        graph_df = graph_df.sort_values(by=['Model', 'Week'])
+    return graph_df
+
+
+# Load the MAE data
+mae_df = pd.read_csv('https://raw.githubusercontent.com/Lambda-School-Labs/nfl-fantasy-ds/master/data/metrics/metrics.csv')
+
+# Convert the MAE data to tidy format
+mae_graph_df = convert_data(mae_df, 'MAE', 'nn', 'arima', 'average')
+
+# Create the MAE line graph
+fig = px.line(mae_graph_df, x='Week', y='MAE', color='Model', render_mode='svg', title='Mean Average Error for Each Model')
+fig.update_xaxes(nticks=17)
+fig.update_layout(title={'x': 0.5, 'xanchor': 'center'})
+
+# Load the percent correct data
+correct_df = pd.read_csv('https://raw.githubusercontent.com/Lambda-School-Labs/nfl-fantasy-ds/master/data/metrics/ban_metrics.csv')
+
+# Convert the percent correct data to tidy format
+pct_graph_df = convert_data(correct_df, 'Percent Correct', 'nn-pct', 'arima-pct', 'avg-pct')
+
+# Create the percent correct graph
+fig1 = px.line(pct_graph_df, x='Week', y='Percent Correct', color='Model', render_mode='svg', title='Percent of Correct Predictions for Each Model')
+fig1.update_xaxes(nticks=17)
+fig1.update_layout(title={'x': 0.5, 'xanchor': 'center'})
+
+layout = html.Div([
+    dcc.Markdown("""
+    ### An Evaluation of the Model Predictions
+
+    """),
+
+    dcc.Graph(figure=fig),
+
+    dcc.Graph(figure=fig1)
+
+    ])
--- tabs/explain.py
+++ tabs/explain.py
@@ -1,9 +1,53 @@
 import dash_core_components as dcc
 
 layout = [dcc.Markdown("""
-#### Explain the Project
+### Methodology
 
-Project background and description
+#### Introduction
 
+NFL fantasy football is a game in which football fans take on the role of the coach or general manager
+of a pro football team.  Partipants draft their initial teams, select players to play each week and trade players
+in order to compete weekly during a season against other teams.  The winning teams are determined by the
+real-life statistics of the pro athletes.
+
+#### Goal of the Trade Analyzer
+
+The goal of the trade analyzer is to determine if a proposed trade of one or more players is a "good"
+or "bad" trade during any week of the 2019 football season.  A good trade is defined as trading a player
+on your team for a player with higher expected fantasy football points.  The goal, therefore, is to determine
+the expected fantasy football points of each pro football player for each week of the 2019 season.  The expected
+points can then be compared between players.
+
+#### The Points System
+
+The trade analyzer uses the ESPN Standard League rules for determining fantasy football points.
+A team consists of 9 starters in 7 position.  The 7 postions are quarterback(QB), running back(RB),
+wide receiver(WR), tight end(TE), kicker(K) and team defense(DF).  A team's defensive squad is
+considered one "player" in fantasy football.
+
+| QB/RB/WR/TE                          | Kickers                         | Defense and Special Teams |
+| -------------------------------------|---------------------------------|---------------------------|
+|6 pts rush or receive TD              |5 pts 50+ yard FG made           |6 pts def/special teams TD |
+|6 pts returning kick/punt for TD      |4 pts 40-49 yard FG made         |2 pts interception         |
+|6 pts player ret/rec fumble for TD    |3 pts FG made 39 yards or less   |2 pts fumble recovery      |
+|4 pts passing TD                      |2 pts rush/pass/rec 2 pt. conv   |2 pts blocked punt/PAT/FG  |
+|2 pts rush/receive 2 pt. conv         |1 pt extra point made            |2 pts safety               |
+|2 pts passing 2 pt. conversion        |-1 pt missed FG                  |4 pts 1-6 points allowed   |
+|1 pts 10 yards rush or receive        |                                 |3 pts 7-13 points allowed  |
+|1 pt 25 yards passing                 |                                 |1 pts 14-17 points allowed |
+|2 pts rush or receive TD of 40+ yds   |                                 |0 pts 18-27 points allowed |
+|2 pts passing TD of 40+ yds           |                                 |-1 pts 28-34 points allowed|
+|-2 pts interception                   |                                 |-3 pts 35-45 points allowed|
+|-2 pts fumble lost                    |                                 |-5 pts 46+ points allowed  |
+|                                      |                                 |1 pt per sack              |
+
+#### Data
+
+The data used for modeling came from *ArmchairAnalysis.com*.  The data consists of over 700+ data points
+over 28 different excel tables providing 20 years of detailed stats for every player in the NFL since the
+year 2000.
+
+The raw data was initially manipulated in Excel in order to link and extract the relevant data for all
+2019 NFL players.  The manipulated data formed the basis for the initial modeling data.
 
 """)]
